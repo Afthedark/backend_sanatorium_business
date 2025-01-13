fedd56bf1644eded863853fb4585631fd9e701e5
Luis Gonzalo <afthedark@hotmail.com>
2025-01-13 08:47:57 -0400

    remove / in all endpoints

--- gestion/urls.py
+++ gestion/urls.py
@@ -22,21 +22,21 @@ router.register('tareas', TareaViewSet)
 
 # Importante: separar las URLs del router y las personalizadas
 custom_urls = [
-    path('tareas/actualizar/', ActualizarTareaEmpleadoAPIView.as_view(), name='actualizar-tarea'),
-    path('registro-empleado/', RegistroEmpleadoAPIView.as_view(), name='registro-empleado'),
-    path('empleados-por-encargado/<int:encargado_id>/', 
+    path('tareas/actualizar', ActualizarTareaEmpleadoAPIView.as_view(), name='actualizar-tarea'),
+    path('registro-empleado', RegistroEmpleadoAPIView.as_view(), name='registro-empleado'),
+    path('empleados-por-encargado/<int:encargado_id>', 
          ListarEmpleadosPorEncargadoAPIView.as_view(), 
          name='empleados-por-encargado'),
-    path('proyectos-por-encargado/<int:encargado_id>/', 
+    path('proyectos-por-encargado/<int:encargado_id>', 
          ListarProyectosPorEncargadoAPIView.as_view(), 
          name='proyectos-por-encargado'),
-    path('proyectos-asignados-empleado/<int:empleado_id>/', 
+    path('proyectos-asignados-empleado/<int:empleado_id>', 
          ListarProyectosAsignadosEmpleadoAPIView.as_view(), 
          name='proyectos-asignados-empleado'),
-    path('tareas-empleado/<int:empleado_id>/', 
+    path('tareas-empleado/<int:empleado_id>', 
          ListarTareasEmpleadoAPIView.as_view(), 
          name='tareas-empleado'),
-    path('tareas-proyecto/<int:proyecto_id>/', 
+    path('tareas-proyecto/<int:proyecto_id>', 
          ListarTareasProyectoAPIView.as_view(), 
          name='tareas-proyecto'),
 
