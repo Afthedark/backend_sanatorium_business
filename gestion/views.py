8975b67e08bc439c7f3e969f043e442b62c515a3
Afthedark <afthedark@hotmail.com>
2025-02-09 17:40:21 -0400

    udpdate info filtro

--- gestion/views.py
+++ gestion/views.py
@@ -581,6 +581,13 @@ class GenerateTasksReportBaseAPIView(APIView):
         elements.append(title)
 
         # Mostrar los filtros aplicados
+        if filtros_aplicados:
+            filters_text = ["Filtros Aplicados:"]
+            for key, value in filtros_aplicados.items():
+                filters_text.append(f"- {key}: {value}")
+            for line in filters_text:
+                elements.append(Paragraph(line, style_normal))
+            elements.append(Spacer(1, 12))
 
         # Información general del proyecto (si se proporciona)
         if proyecto:
